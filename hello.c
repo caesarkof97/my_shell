16f778b1dd691e45fe1b004deea8068cee68a369
Caesar <caesarkof97@163.com>
2016-12-22 16:46:31 +0800

    hello

--- hello.c
+++ hello.c
@@ -7,9 +7,19 @@
 
 #include<stdio.h>
 extern int valueB, valueA;
+extern char __executable_start[];
+extern char etext[], _etext[], __etext[];
+extern char edata[], _edata[];
+extern char end[], _end[];
 int add(int, int);
 int main(){
 	valueB = 200;
 	printf("Hello World!\n%d\n", add(valueA, valueB));
+	printf("Executable Start %x\n", __executable_start);
+	printf("Text End %x %x %x\n", etext, _etext, __etext);
+	printf("Data End %x %x \n", edata, _edata);
+	printf("Executable End %x %x\n", end, _end);
+	int c = valueA - valueB;
+	printf("%d\n", c);
 	return 0;
 }
